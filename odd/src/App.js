45e9b727b2cd0c94cce4b3fbe382913e0fce8c3e
Han Jeonghoon <gkswjdgns59@kaist.ac.kr>
2022-05-20 18:46:44 +0900

    initialize

---
+++ odd/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      test text
+    </div>
+  );
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const http = require('http');
+
+const port = process.env.PORT || process.env.port || 5000;
+
+const app = express();
+const server = http.createServer(app);
+const PORT = 5000;
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
