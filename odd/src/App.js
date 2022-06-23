eea818bc623d1fb3896c62fcc44eb05ef372593e
Davyd_kim <kdo4020@naver.com>
2022-06-24 07:15:57 +0900

    home intro final

--- odd/src/App.js
+++ odd/src/App.js
@@ -6,12 +6,26 @@ import Test6 from './components/Test6/Test6';
 
 import './App.css';
 import { SceneHallway } from './components/game-elements/Scenes';
+import { motion, useViewportScroll, useTransform } from "framer-motion";
 import Phaser from "phaser";
 import 'react-notion/src/styles.css';
 import { NotionRenderer } from 'react-notion';
-import { useEffect, useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import axios from 'axios';
+import styles from './App.module.css';
 
+import IMG_left1 from './components/images/left1.png';
+import IMG_left2 from './components/images/left2.png';
+import IMG_left3 from './components/images/left3.png';
+import IMG_left4 from './components/images/left4.png';
+import IMG_right1 from './components/images/right1.png';
+import IMG_right2 from './components/images/right2.png';
+import IMG_right3 from './components/images/right3.png';
+import IMG_right4 from './components/images/right4.png';
+import IMG_hotel from './components/images/hotel.png';
+import IMG_pot from './components/images/hotel_pot.png';
+import IMG_letter from './components/images/letter.png';
+import IMG_button from './components/images/button.png';
 
 const config = {
   type: Phaser.AUTO,
@@ -38,7 +52,24 @@ const game = new Phaser.Game(config);
 function App() {
   const [response, setResponse] = useState({});
   const [enter, setEnter] = useState(false);
+  const { scrollYProgress } = useViewportScroll();
+
+  const xLeft1 = useTransform(scrollYProgress, [0.2, 1], ["0px", "-4000px"]);
+  const xLeft2 = useTransform(scrollYProgress, [0.35, 1], ["0px", "-4000px"]);
+  const xLeft3 = useTransform(scrollYProgress, [0.5, 1], ["0px", "-4000px"]);
+  const xLeft4 = useTransform(scrollYProgress, [0.65, 1], ["0px", "-4000px"]);
   
+  const xRight1 = useTransform(scrollYProgress, [0.2, 1], ["0px", "4000px"]);
+  const xRight2 = useTransform(scrollYProgress, [0.35, 1], ["0px", "4000px"]);
+  const xRight3 = useTransform(scrollYProgress, [0.5, 1], ["0px", "4000px"]);
+  const xRight4 = useTransform(scrollYProgress, [0.65, 1], ["0px", "4000px"]);
+
+  const scaleHotel = useTransform(scrollYProgress, [0.6, 1], ["0.3","1"]);
+
+  const yLetter = useTransform(scrollYProgress, [0.05, 0.35], ["0","-100vh"]);
+  const scaleLetter = useTransform(scrollYProgress, [0, 0.15], ["1","0.7"]);
+
+
   useEffect(() => {
     const NOTION_PAGE_ID = 'be4ddb06be8d423785dcd8a4372d1e05';
     axios
@@ -47,8 +78,17 @@ function App() {
         setResponse(data);
       })
   }, []);
+
+
+  useEffect(() => {
+    scrollYProgress.onChange((latest) => {
+      console.log(latest);
+    });
+  }, []);
+
+
   return (
-    <div className="App">
+    <div className={styles.App}>
       {/* {Object.keys(response).length && (
         <NotionRenderer blockMap={response} fullPage={true} />
       )} */}
@@ -57,7 +97,114 @@ function App() {
         
       </div> */}
       {/* <Test5/> */}
-      <Test6/>
+      {/* <Test6/> */}
+      <div className={styles.container}>
+        <motion.div
+          className={styles.hotel_box}
+          style={{
+            scale: scaleHotel
+          }}
+        >
+          <img src={IMG_hotel}></img>
+        </motion.div>
+
+
+        <motion.div
+          className={styles.box}
+          style={{
+            x: xLeft4
+          }}
+        >
+          <img src={IMG_left4}></img>
+        </motion.div>
+        <motion.div
+          className={styles.box}
+          style={{
+            x: xRight4
+          }}
+        >
+          <img src={IMG_right4}></img>
+        </motion.div>
+
+
+        <motion.div
+          className={styles.box}
+          style={{
+            x: xLeft3
+          }}
+        >
+          <img src={IMG_left3}></img>
+        </motion.div>
+        <motion.div
+          className={styles.box}
+          style={{
+            x: xRight3
+          }}
+        >
+          <img src={IMG_right3}></img>
+        </motion.div>
+
+
+        <motion.div
+          className={styles.box}
+          style={{
+            x: xLeft2
+          }}
+        >
+          <img src={IMG_left2}></img>
+        </motion.div>
+        <motion.div
+          className={styles.box}
+          style={{
+            x: xRight2
+          }}
+        >
+          <img src={IMG_right2}></img>
+        </motion.div>
+
+
+        <motion.div
+          className={styles.box}
+          style={{
+            x: xLeft1
+          }}
+        >
+          <img src={IMG_left1}></img>
+        </motion.div>
+        <motion.div
+          className={styles.box}
+          style={{
+            x: xRight1
+          }}
+        >
+          <img src={IMG_right1}></img>
+        </motion.div>
+
+        <motion.div
+          className={styles.letter_box}
+          style={{
+            y: yLetter,
+            scale: scaleLetter
+          }}
+        >
+          <motion.div
+            className={styles.button}
+            // onClick={() => {
+              //   setIsClick(!isClick)
+            // }}
+            whileHover={{scale: 1.1}}
+            whileTap={{scale: 0.9}}
+            transition={{
+              duration: 0.3
+            }}
+          >
+              SCROLL
+          </motion.div>
+          <img src={IMG_letter}></img>
+        </motion.div>
+
+
+      </div>
       <Header/>
     </div>
   );
