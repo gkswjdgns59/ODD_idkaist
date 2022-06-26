3851872080df8cae7525d24b78198648154c85ab
Han Jeonghoon <gkswjdgns59@kaist.ac.kr>
2022-06-27 03:19:47 +0900

    g

--- odd/src/socket-client.js
+++ odd/src/socket-client.js
@@ -1,7 +1,7 @@
 import socketIOClient from 'socket.io-client';
 
 //hosting 이후 url 포함하도록 수정 필요
-const ENDPOINT = "http://localhost:4000" || "https://odd-idkaist.herokuapp.com/"
+const ENDPOINT = "https://odd-idkaist.herokuapp.com/"
 
 const socket = socketIOClient(ENDPOINT);
 export default socket;
