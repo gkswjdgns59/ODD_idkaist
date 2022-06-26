b3ae24a86787901dda7a7641048a489bd3c8a883
Davyd_kim <kdo4020@naver.com>
2022-06-26 16:01:47 +0900

    elevator (davyd -> junghoon)

--- odd/src/components/Elevator.js
+++ odd/src/components/Elevator.js
@@ -10,10 +10,10 @@ export default function Elevator() {
         <motion.div className={styles.elevator_container}>
             <img src={IMG_elevator}></img>
             <div className={styles.floor}>
-                <li>1 - LOBBY</li>
-                <li>2 - ID200</li>
-                <li>3 - ID300</li>
                 <li>4 - ID400</li>
+                <li>3 - ID300</li>
+                <li>2 - ID200</li>
+                <li>1 - LOBBY</li>
             </div>
         </motion.div>
     )
